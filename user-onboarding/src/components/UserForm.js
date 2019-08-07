b75d3c1cd6f411171cc612aca3b1bbf856b110cb
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-08-07 15:38:19 -0400

    validation complete

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
-const UserForm = props => {
+const UserForm = ({ touched, errors }) => {
   return (
     <div>
       <h1>User Form</h1>
       <Form>
         <Field type="text" name="name" placeholder="Name..." />
+        {touched.name && errors.name && <p>{errors.name}</p>}
         <Field type="email" name="email" placeholder="email@email.com" />
+        {touched.email && errors.email && <p>{errors.email}</p>}
         <Field type="password" name="password" placeholder="Password..." />
+        {touched.password && errors.password && <p>{errors.password}</p>}
         <Field type="checkbox" name="terms" placeholder="Terms of Service" />
-        <submit>Submit</submit>
+        {touched.terms && errors.terms && <p>{errors.terms}</p>}
+        <button type="submit">Submit</button>
       </Form>
     </div>
   );
@@ -24,6 +29,15 @@ const FormikUserForm = withFormik({
       password: password || "",
       terms: terms || ""
     };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required(),
+    password: Yup.string().required(),
+    terms: Yup.bool().required()
+  }),
+  handleSubmit(values) {
+    console.log(values);
   }
 })(UserForm);
 
